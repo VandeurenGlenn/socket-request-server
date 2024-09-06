956f97dd17572252aafd78cc0e3e174cbb2349cf
VandeurenGlenn <8685280+VandeurenGlenn@users.noreply.github.com>
2024-09-06 19:24:29 +0200

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,20 @@
-import typescript from '@rollup/plugin-typescript';
-import tsconfig from './tsconfig.json' assert { type: 'json'};
+import typescript from '@rollup/plugin-typescript'
 
 try {
   rimraf('rm ./dist/*.js')
 } catch (e) {
-  console.log('nothing to clean');
-};
+  console.log('nothing to clean')
+}
 
-export default  [{
-  input: ['src/server.ts', 'src/response.ts/', 'src/connection.ts'],
-  output: [{
-    dir: './exports',
-    format: 'es'
-  }],
-  plugins: [
-    typescript()
-  ]
-}]
+export default [
+  {
+    input: ['src/server.ts', 'src/response.ts/', 'src/connection.ts'],
+    output: [
+      {
+        dir: './exports',
+        format: 'es'
+      }
+    ],
+    plugins: [typescript()]
+  }
+]
